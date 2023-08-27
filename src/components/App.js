838746e532329d16435d349b9450152f2dd91689
+ <+>
2023-08-26 18:18:50 -0600

    error

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
+import Links from "./Links"; 
 import user from "../data/user";
 console.log(user);
 
@@ -11,8 +10,12 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About
+        bio={user.bio}
+       links={user.links}
+      />
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
